4db0f97c592b93945e30d9bf34bd38f65c859a6e
Dan <mr.danielross@gmail.com>
2023-10-06 14:24:42 +0100

    Add rooms components

---
+++ src/components/rooms/index.tsx
@@ -0,0 +1,23 @@
+import Room, { RoomProps } from '@/components/room';
+import styles from './rooms.module.css';
+const Rooms = async () => {
+  const roomsData = await fetch('https://wetransfer.github.io/rooms.json').then(
+    (data) => data.text()
+  );
+
+  if (!roomsData) return <>Error</>;
+
+  const parsedRoomsData: RoomProps[] = JSON.parse(roomsData).rooms;
+
+  if (!parsedRoomsData) return <>Error</>;
+
+  return (
+    <menu className={styles.cards}>
+      {parsedRoomsData.map(({ name, spots, thumbnail }) => (
+        <Room name={name} spots={spots} thumbnail={thumbnail} key={name} />
+      ))}
+    </menu>
+  );
+};
+
+export default Rooms;
