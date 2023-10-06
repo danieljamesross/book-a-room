db4f6f8b9f12a5a73256b0bfbe32dddcdbb11011
Dan <mr.danielross@gmail.com>
2023-10-06 14:24:17 +0100

    Add button component

---
+++ src/components/ui/button/index.tsx
@@ -0,0 +1,10 @@
+import { HTMLProps } from 'react';
+import styles from './button.module.css';
+
+const Button = ({ onClick, children }: HTMLProps<HTMLButtonElement>) => (
+  <button className={styles.button} onClick={onClick}>
+    {children}
+  </button>
+);
+
+export default Button;
